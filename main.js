58caf06946e03cd5fffb37dd3e739ac099e1efa4
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-21 17:29:52 +0330

    remove va add baraye link va burgerbuton

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const burgerButton = document.querySelector(".burger-btn-wrapper");
 const headerNav = document.querySelector(".header-nav");
-const navLinks = document.querySelector(".nav-link");
+const navLinks = document.querySelectorAll(".nav-link");
 
 
 ///TOGLE NAVBAR MENu NEW WE ClICK BURGEr BURGER BUTON
 burgerButton.addEventListener('click',()=>{
     burgerButton.classList.toggle("open-btn");
     headerNav.classList.toggle("open-list");
-})
+});
+
+navLinks.forEach(link=>{
+   link.addEventListener('click', () => {
+    burgerButton.classList.remove("open-btn");
+    navLinks.classList.add("open-list");
+   });
+});
