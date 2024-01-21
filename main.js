fd8e5940cb6682e3796d2937f271aad8b8a5db5d
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-21 17:05:52 +0330

    Event click baraye burger button

--- main.js
+++ main.js
@@ -1,2 +1,8 @@
 const burgerButton = document.querySelector(".burger-btn-wrapper");
 const headerNav = document.querySelector(".header-nav");
+
+
+///TOGLE NAVBAR MENu NEW WE ClICK BURGEr BURGER BUTON
+burgerButton.addEventListener('click',()=>{
+    burgerButton.classList.toggle(".open-btn");
+})
