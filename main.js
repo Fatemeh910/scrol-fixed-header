d72aa3a6f9c7102df4c7ec1671ed473b856cd68c
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-21 17:16:45 +0330

    Element nav link

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
 const burgerButton = document.querySelector(".burger-btn-wrapper");
 const headerNav = document.querySelector(".header-nav");
+const navLinks = document.querySelector(".nav-link");
 
 
 ///TOGLE NAVBAR MENu NEW WE ClICK BURGEr BURGER BUTON
 burgerButton.addEventListener('click',()=>{
-    burgerButton.classList.toggle(".open-btn");
+    burgerButton.classList.toggle("open-btn");
     headerNav.classList.toggle("open-list");
 })
