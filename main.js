4afe6d8faa3d818e622e9d7de6e44225e7f3452e
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-22 17:44:51 +0330

    End comit

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
+// ELEMENTS
 const burgerButton = document.querySelector(".burger-btn-wrapper");
-const header = document.querySelectorAll(".header");
+const header = document.querySelector(".header");
 const headerNav = document.querySelector(".header-nav");
 const navLinks = document.querySelectorAll(".nav-link");
 
-///TOGLE NAVBAR MENu NEW WE ClICK BURGEr BURGER BUTON
-burgerButton.addEventListener('click', () => {
-    burgerButton.classList.toggle("open-btn");
-    headerNav.classList.toggle("open-list");
+// TOGGLE NAVBAR MENU WHEN WE CLICK BURGER BUTTON
+burgerButton.addEventListener("click", () => {
+  burgerButton.classList.toggle("open-btn");
+  headerNav.classList.toggle("open-list");
 });
 
-// SET LIST TO CLOSE WHEN WE CLICK TO EVERY HEADER LINKSمخصوص بستن لیست
+// SET LIST TO CLOSE WHEN WE CLICK TO EVERY HEADER LINKS
 navLinks.forEach((link) => {
-    link.addEventListener("click", () => {
-      burgerButton.classList.remove("open-btn");
-      headerNav.classList.remove("open-list");
-    });
+  link.addEventListener("click", () => {
+    burgerButton.classList.remove("open-btn");
+    headerNav.classList.remove("open-list");
   });
-  
-  navLinks.forEach((link) => {
-    link.addEventListener("click", function (e) {
-      e.preventDefault();
-  
-      // GET CURRENT SECTION
-      const targetId = this.getAttribute("href").substring(1);
-      const targetSection = document.getElementById(targetId);
-  
-      // GET HEADER HEIGHT
-      const headerHeight = parseInt(window.getComputedStyle(header).height);
-  
-      // SCROLL TO SECTION THAT WE CLLICKED IN ITS LINKK
-      if (targetSection) {
-        window.scrollTo({
-          top: targetSection.offsetTop - headerHeight,
-          behavior: "smooth",
-        });
-      }
-    });
+});
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // GET CURRENT SECTION
+    const targetId = this.getAttribute("href").substring(1);
+    const targetSection = document.getElementById(targetId);
+
+    // GET HEADER HEIGHT
+    const headerHeight = parseInt(window.getComputedStyle(header).height);
+
+    // SCROLL TO SECTION THAT WE CLLICKED IN ITS LINKK
+    if (targetSection) {
+      window.scrollTo({
+        top: targetSection.offsetTop - headerHeight,
+        behavior: "smooth",
+      });
+    }
   });
-  
+});
