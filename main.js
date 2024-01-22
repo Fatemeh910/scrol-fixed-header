3af0212546927788eb43276708f9da6b29aa7c99
Fatemeh910 <fatemehkhoshnod91@gmail.com>
2024-01-22 17:09:26 +0330

    Shart if

--- main.js
+++ main.js
@@ -4,33 +4,34 @@ const navLinks = document.querySelectorAll(".nav-link");
 const header = document.querySelectorAll(".header");
 
 ///TOGLE NAVBAR MENu NEW WE ClICK BURGEr BURGER BUTON
-burgerButton.addEventListener('click',()=>{
+burgerButton.addEventListener('click', () => {
     burgerButton.classList.toggle("open-btn");
     headerNav.classList.toggle("open-list");
 });
 
 ///SET LIST TO CLOSE WHEN WE CLICK TO EVERY HEADER LINKSمختص بستن لیست
 navLinks.forEach((link) => {
-   link.addEventListener('click', () => {
-    burgerButton.classList.remove("open-btn");
-    headerNav.classList.remove("open-list");
-   });
-});
-
-navLinks.forEach(link =>{
-    link.addEventListener('click', function (e) {
-     e.preventDefault();    
-
-//GET CURENT SECTIONگرفتن سکشن فعلی
-     const targetId = this.getAttribute("href").substring(1);
-     const targetSection = document.getElementById(targetId);
-
-//GET HEADER HEIGEگرفتن ارتفاع هدر
-   const headerHeight = parseInt(window.getComputedStyle(header).height);
-//SCROLL TO SECTIN THAT WEاسکروا کن به سکشنی که روی لینکش کلیک کردیم
-     window.scrollTo({
-        top: targetSection.offsetTop - headerHeight,
-        behavior: "smooth",
-     });
+    link.addEventListener('click', () => {
+        burgerButton.classList.remove("open-btn");
+        headerNav.classList.remove("open-list");
     });
 });
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        //GET CURENT SECTIONگرفتن سکشن فعلی
+        const targetId = this.getAttribute("href").substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        //GET HEADER HEIGEگرفتن ارتفاع هدر
+        const headerHeight = parseInt(window.getComputedStyle(header).height);
+        //SCROLL TO SECTIN THAT WEاسکروا کن به سکشنی که روی لینکش کلیک کردیم
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop - headerHeight,
+                behavior: "smooth",
+            });
+        }
+      });
